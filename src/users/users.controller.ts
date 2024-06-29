1278d1b14885ff340190ec5f21071f8cf4f8e056
Amindu Sathsara <dapsathsara@std.appsc.sab.ac.lk>
2024-06-30 00:14:32 +0530

    adding validation pipe to validate incomming request

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,7 @@
-import { Body, Controller, Delete, Get,Param,Patch,Post,Query,ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get,Param,Patch,Post,Query,ParseIntPipe,ValidationPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { createUserDto } from './dto/create-user.dto';
-import { updateUserDto } from './dto/update-user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 
 
@@ -39,13 +39,13 @@ findOne(@Param('id',ParseIntPipe) id: number){
 
 
 @Post()
-create(@Body() user:{name:string,email:string,role:'INTERN'|'ENGINEER'|'ADMIN'}){
-    return this.usersService.create(user)
+create(@Body(ValidationPipe) createUserDto:CreateUserDto){
+    return this.usersService.create(createUserDto);
 }
 
 @Patch(':id')      //patch  /users/:id
-update(@Param('id',ParseIntPipe) id:number, @Body() userUpdate:{name?:string,email?:string,role?:'INTERN'|'ENGINEER'|'ADMIN'}){
-    return this.usersService.update(id,userUpdate)
+update(@Param('id', ParseIntPipe) id: number, @Body(ValidationPipe) updateUserDto:UpdateUserDto){
+    return this.usersService.update(id, updateUserDto)
 }
 
 
