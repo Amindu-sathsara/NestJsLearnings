33a24264215cebbb307ffdcd167b11e3aaef06af
Amindu Sathsara <dapsathsara@std.appsc.sab.ac.lk>
2024-06-30 10:53:59 +0530

    Adding validation and also if user do bad request set NotfoundExeption and create custom massage for better experiance for the user

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,6 +1,6 @@
-import { createUserDto } from "./create-user.dto";
+import { CreateUserDto } from "./create-user.dto";
 import {PartialType} from "@nestjs/mapped-types";   //before this run this command   :npm i @nestjs/mapped-types -D                                                             
 
 // using PartialType  instead of creating upadate-user.dto.ts seperately we can use the extends method because the nestjs also work with js class basically
 
-export class UpdateUserDto extends PartialType(createUserDto){}
+export class UpdateUserDto extends PartialType(CreateUserDto){}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
-
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { NotFoundException } from '@nestjs/common';
 @Injectable()
 export class UsersService {
-    private users=[
+    private users = [
         {
             id: 1,
             name: 'John',
@@ -13,59 +15,56 @@ export class UsersService {
             id: 2,
             name: 'Jane',
             email: 'jane@gmail.com',
-            role    : 'ENGINEER'
+            role: 'ENGINEER'
         },
         {
             id: 3,
             name: 'Jack',
             email: 'jack@gmail.com',
             role: 'INTERN'
-        } 
-    ]
+        }
+    ];
 
-
-    //findAll methods is provide service for requesting  getting  for all the user data
-    findAll(role?:'INTERN'|'ENGINEER'|'ADMIN'){
-        if(role){
-            return this.users.filter(user=>user.role===role)
+    // Method to get all users or filter by role
+    findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+        if (role) {
+            return this.users.filter(user => user.role === role);
         }
         return this.users;
-
     }
-    findOne(id:number){
-        const user=this.users.find(user=>user.id===id);
+
+    findOne(id: number) {
+        const user = this.users.find(user => user.id === id);
+        if(!user) throw new NotFoundException("That user is not found");
         return user;
     }
 
-
-    //creating services for post requests
-    create(user:{name:string,email:string,role:'INTERN'|'ENGINEER'|'ADMIN'}){
-        const userByHighestId=[...this.users].sort((a,b)=>b.id-a.id);
-        const newUser={
-            id:userByHighestId[0].id+1,
-            ...user
-        }
+    // Method to create a new user
+    create(createUserDto: CreateUserDto) {
+        const userByHighestId = [...this.users].sort((a, b) => b.id - a.id);
+        const newUser = {
+            id: userByHighestId[0].id + 1,
+            ...createUserDto
+        };
         this.users.push(newUser);
         return newUser;
     }
 
-    update(id:number,updateUser:{name?:string,email?:string,role?:'INTERN'|'ENGINEER'|'ADMIN'}){
-        this.users=this.users.map(user=>{
-            if(user.id==id){
-                return {...user,...updateUser};
-        }
-        return user;
-    })
-
-    return this.findOne(id);    
-}
-    delete(id:number){
-        const removerdUser=this.findOne(id);
-
-        this.users=this.users.filter(user=>user.id!==id); //filter the user array here..
-        return removerdUser;  //return that removed user
-
+    // Method to update an existing user
+    update(id: number, updateUserDto: UpdateUserDto) {
+        this.users = this.users.map(user => {
+            if (user.id === id) {
+                return { ...user, ...updateUserDto };
+            }
+            return user;
+        });
+        return this.findOne(id);
     }
 
-
+    // Method to delete a user
+    delete(id: number) {
+        const removedUser = this.findOne(id);
+        this.users = this.users.filter(user => user.id !== id);
+        return removedUser;
+    }
 }
